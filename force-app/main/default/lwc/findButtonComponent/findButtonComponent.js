13fcbfd8e7818a4d97831acb9ffdc6bed1b281d9
facostap <acostamichael80@gmail.com>
2024-03-20 00:38:13 -0500

    clean files

--- force-app/main/default/lwc/findButtonComponent/findButtonComponent.js
+++ force-app/main/default/lwc/findButtonComponent/findButtonComponent.js
@@ -1,20 +1,26 @@
 /**
  * @author            : Fernando M. Acosta P.
  * @Equipo            : CRM
- * @last modified on  : 02-24-2024
+ * @last modified on  : 03-19-2024
  * @last modified by  : Fernando M. Acosta P.
  * @Descripción       : Componente o clase apex ...
 **/
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track} from 'lwc';
+import getMarsPhotos from '@salesforce/apex/MarsPhotoCallout.obtainNasaMarsPhotos';
 
 export default class FindButtonComponent extends LightningElement {
     @api receivedArray;
     arrayValuesString;
     @track newArray = [];
+    @track isLoading = false;
+
     handleQuery() {
+
+        this.isLoading = true;
+
         this.arrayValuesString = JSON.stringify(this.receivedArray); // esto sirve para pasar a String
         if (this.receivedArray && this.receivedArray.length) {
-            console.log('SIZE DEL ARRAY: ',this.receivedArray.length);
+            console.log('SIZE DEL ARRAY: ', this.receivedArray.length);
             console.log('ARRAY OBTENIDO: ', this.receivedArray.join(', '));
             console.log('ARRAY PARA ITERAR: ', this.receivedArray);
             this.receivedArray.forEach((element, index) => {
@@ -23,11 +29,30 @@ export default class FindButtonComponent extends LightningElement {
                     this.newArray.push(element);
                 }
             });
-            console.log('CLEAN ARRAY: ',this.newArray);
-            console.log('CLEAN ARRAY: ',this.newArray.join(', '));
+            console.log('CLEAN ARRAY: ', this.newArray);
+            console.log('CLEAN ARRAY: ', this.newArray.join(', '));
+
         } else {
             console.log('No existen registros');
         }
+        getMarsPhotos({nameRover:this.newArray[0], nameCamera:this.newArray[1], numSol:this.newArray[2]})
+        .then(result => {
+            // Manejar el resultado aquí
+            console.log('>>>>>>>>',JSON.stringify(result));
+        })
+        .catch(error => {
+            // Manejar errores aquí
+            console.error('>>>+++>>>',error);
+        })
+        .finally(()=> {
+            this.isLoading = false; // Ocultar mensaje de carga
+        });
+        console.log(this.newArray);
+        console.log(this.newArray[0]);
+        console.log(this.newArray[1]);
+        console.log(this.newArray[2]);
+
+        return this.newArray;
     }
 
 }
